d5b6a28bbd0c9e179eac72c5cab3a5d70c9e28cc
myquery <stanarua@aol.com>
2020-05-06 23:31:44 +0100

    feature(sendmail): save

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser')
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const sendMail = require('./routes/sendmail');
 
 const app = express();
 
@@ -13,8 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/send', sendMail);
 
 module.exports = app;
