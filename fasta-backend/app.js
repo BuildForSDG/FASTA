8cc962e0934287f70400c28a0cec5136c3a45228
myquery <stanarua@aol.com>
2020-05-06 23:55:35 +0100

    feature

--- fasta-backend/app.js
+++ fasta-backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser')
 
 
