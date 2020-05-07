5628836792d53327a749e1a5b2ecba66935fce96
myquery <stanarua@aol.com>
2020-05-07 21:35:28 +0100

    bug(send-mail): conflict resolve merge conflict

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 
 const indexRouter = require('./routes/index');
