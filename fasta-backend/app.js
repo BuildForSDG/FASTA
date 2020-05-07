1041abb62aeb55f8b2e762eaf95115b628902c58
obafemi oderanti <thevetdoctor.gmail.com>
2020-05-07 14:53:19 +0100

    fix codacy feedback

--- fasta-backend/app.js
+++ fasta-backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('./db/index');
+require("./db/index");
 
 const app = express();
 
