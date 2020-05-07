9c6ce8776ae31223cf52457a703455b60a836bd9
obafemi oderanti <thevetdoctor.gmail.com>
2020-05-07 14:53:19 +0100

    fix codacy feedback

--- db/index.js
+++ db/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-console */
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 
 dotenv.config();
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useCreateIndex', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.set("useCreateIndex", true);
 
 mongoose.connect(process.env.DB_URI)
-  .then(() => console.log('FASTA DB connected!'))
+  .then(() => console.log("FASTA DB connected!"))
   .catch((error) => console.error(error.message));
 
 module.exports = mongoose;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   fullname: {
@@ -19,4 +19,4 @@ const userSchema = mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
