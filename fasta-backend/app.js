06ee73612b9b86bcc12e16e25d2f49e1a196e6b6
myquery <stanarua@aol.com>
2020-05-09 07:04:06 +0100

    test(send mail): registration correct the eslint error on codebase#20

--- fasta-backend/app.js
+++ fasta-backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/", indexRouter);
-app.use("/api/v1/send", sendRouter);
+app.use("/api/v1/", sendRouter);
 
 
 module.exports = app;
