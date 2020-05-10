349eb46d69791fd2184c1b5b5a214804f0345778
myquery <stanarua@aol.com>
2020-05-10 01:10:49 +0100

    test(send mail): registration update codebase to send mail in gmail and allow access#20

--- app.js
+++ app.js
@@ -2,27 +2,31 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const bodyParser = require("body-parser");
+
 const userRouter = require("./routes/users");
 const indexRouter = require("./routes/index");
 const sendRouter = require("./routes/sendmail");
+
 require("./db/index");
 // const userRouter = require("./routes/index");
-
 
 const app = express();
 // app.use(userRouter);
 // indexRouter(app);
 // app.use(indexRouter);
 
+app.use(bodyParser.json({ type: "application/json" }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/", indexRouter);
 app.use("/api/v1/", sendRouter);
-
 
 module.exports = app;
