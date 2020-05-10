e418274fcd9631c58d1ec7d37478e1226cf233ae
myquery <stanarua@aol.com>
2020-05-10 02:07:14 +0100

    test(send mail): registration update codebase to send mail in gmail and allow access#20

--- fasta-backend/routes/sendmail.js
+++ fasta-backend/routes/sendmail.js
@@ -3,45 +3,36 @@
 /* eslint-disable prefer-const */
 const express = require("express");
 const nodemailer = require("nodemailer");
+const smtpTransport = require("nodemailer-smtp-transport");
 
 const router = express.Router();
 
-
-router.use("/send", (req, res, next) => {
-  console.log("router middleware called");
-  console.log(req.method, req.url);
-  next();
-});
-
 router.post("/send", async (req, res) => {
-  if (!req.body || req.body.length === 0) {
-    console.log("request body not found");
-    return res.sendStatus(400);
-  }
   // what will be sent to the registrants after registering
-  const output = `<div style='margin: 0 auto; background: #ededed; border-top:2px solid green; border-bottom:2px solid green; box-shadow: 1px 2px 3px 4px #ccc; padding: 20px; width:60%; height:60% '>
+  const output = `<div style='margin: 0 auto; background: #ededed; border-top:2px solid green; border-bottom:2px solid green; box-shadow: 1px 2px 3px 4px #ccc; padding: 1.5rem; width: 50%; padding: 20px; height: auto'>
           <h1>Hey! ${req.body.fullname} Welcome to Fasta</h1>
           <hr>
-          <p style='padding:1.5rem;'>FASTA helps you plan your Trip and allow you to go faster, click this <a href='#'>Link</a> to confirm your registration</p>
+          <div style='padding:1.5rem; margin:15px'>
+          <p>FASTA helps you plan your Trip and allow you to go faster, click this <a href='#'>Link</a> to confirm your registration</div>
+          </div>
+          <br>
+          <br>
           <h4>Welcome on board</h4>
           </div>
           `;
   // Nodemailer setup
-  let fastaMailer = await nodemailer.createTransport({
+  let fastaMailer = await nodemailer.createTransport(smtpTransport({
     service: "gmail",
-    secure: false,
+    host: "smtp.gmail.com",
     auth: {
       user: "fastamovement@gmail.com", // gmail created just for testing purposes
       pass: "Fasta123" // for testing purposes
-    },
-    tls: {
-      rejectUnauthorized: false
     }
-  });
+  }));
 
   let mailOptions = {
     from: "'Welcome to FASTA' <fastamovement@gmail.com>",
-    to: "stanarua@aol.com", // "stanarua@aol.com",
+    to: `${req.body.email}`,
     subject: "Your Registration was successful",
     text: "Hey there, it’s our first message from FASTA Team",
     html: output
@@ -52,7 +43,6 @@ router.post("/send", async (req, res) => {
     }
     console.log("Message sent: %s", info.messageId);
     res.send(`Email sent to ${req.body.email}`);
-    console.log(`${req.body.email}`);
   });
 });
 
