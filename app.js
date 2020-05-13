6c2ef86a3aa1c05b4b4d7bd8543e445ec5582ec9
obafemi oderanti <thevetdoctor.gmail.com>
2020-05-13 09:31:33 +0100

    integrate front and back ends

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const bodyParser = require("body-parser");
+const CORS = require('cors');
 
 const userRouter = require("./routes/users");
 const indexRouter = require("./routes/index");
@@ -24,11 +25,14 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(CORS()); 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/", indexRouter);
 app.use("/api/v1/", sendRouter);
 app.use("/api/v1/", getTransporter);
+
+app.get("/", (req, res) => res.send(`<h2>Welcome to FASTA</h2><p><a href="/api/v1">Check out version 1 of the FASTA API</a></p>`));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const express = require("express");
 
 const router = express.Router();
 
+router.get("/", (req, res) => res.status(200).json({ message: 'Welcome to FASTA, taking you faster in safety & convenience!'}));
+
 module.exports = router;
