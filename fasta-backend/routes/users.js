8076542bad7a3553ce23d36bb3a43e2691d2de65
obafemi oderanti <thevetdoctor.gmail.com>
2020-05-13 09:31:33 +0100

    integrate front and back ends

--- fasta-backend/routes/users.js
+++ fasta-backend/routes/users.js
@@ -8,13 +8,11 @@ const authChecker = require("../middlewares/authChecker");
 
 //  CREATE A NEW USER AND ADD TO DATABASE
 router.post("/", async (req, res) => {
-  // console.log(req.body);
   // eslint-disable-next-line consistent-return
-  await User.findOne({ email: req.body.email }).then((result) => {
-    if (result) {
+  const userExist = await User.findOne({ email: req.body.email });
+    if (userExist) {
       return res.status(403).json({ response: "email exists" });
     }
-  });
   try {
     const {
       fullname, email, phonenumber, password, confirmPassword
@@ -26,6 +24,7 @@ router.post("/", async (req, res) => {
     await User.create({
       fullname, email, phonenumber, password: hash
     });
+    console.log("Signup succesfully");
     return res.status(200).json({ response: "Signup succesfully" });
   } catch (error) {
     return res.status(500).json({ response: error.message });
@@ -48,7 +47,7 @@ router.post("/login", async (req, res) => {
       if (passwordcheck) {
         const token = bcrypt.generateToken(user);
         return res.status(200).json({
-          response: "Login succesfull",
+          response: "Login successful",
           token
         });
       }
@@ -71,7 +70,7 @@ router.get("/", authChecker, (req, res) => {
   );
 });
 
-//  GET A SPECIFIC USER BY ID) FROM DATABASE
+//  GET A SPECIFIC USER (BY ID) FROM DATABASE
 router.get("/:id", authChecker, (req, res) => {
   const { id } = req.params;
 
