9968568a8db9f1174d09ae4cf060668011584980
myquery <stanarua@aol.com>
2020-05-14 13:32:19 +0100

    feat(trip): trip api to return trip information

--- fasta-backend/api/transporters-api.js
+++ fasta-backend/api/transporters-api.js
@@ -1,13 +1,15 @@
+/* eslint-disable max-len */
 /* eslint-disable no-console */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const https = require("https");
+
 // Option parameter for google api request
 const options = {
   hostname: "maps.googleapis.com",
   port: 443,
-  path: `/maps/api/place/textsearch/json?query=transport&location=${this.latitude},${this.longitude}&radius=10000&key=${JSON_stringify(process.env.TEST_KEY)}`,
+  path: `/maps/api/place/textsearch/json?query=transport&location=${this.latitude},${this.longitude}&radius=10000&key=AIzaSyC8G-WQhDTvlVflIFPzOj30rDQGk0Fekx8`,
   method: "GET"
 };
 
@@ -16,6 +18,12 @@ function GetTransporter(latitude, longitude) {
   this.latitude = latitude;
   this.longitude = longitude;
 }
+
+// COmpute polygon
+GetTransporter.prototype.computePolygon = () => {
+// empty function
+// const google = new google.maps
+};
 
 // Get nearby transport 1km away
 GetTransporter.prototype.getPlaces = async () => {
@@ -31,6 +39,8 @@ GetTransporter.prototype.getPlaces = async () => {
 
   req.end();
 };
+
+//
 
 
 module.exports = GetTransporter;
--- fasta-backend/routes/location-transporter.js
+++ fasta-backend/routes/location-transporter.js
@@ -2,16 +2,30 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 const express = require("express");
+const { Client, Status } = require("@googlemaps/google-maps-services-js");
 const Transporters = require("../api/transporters-api");
+const { TripInfo, TripMetrix } = require("../api/trip-api");
+
 
 const router = express.Router();
 
 // api to get nearby transporters base on users location
 router.post("/location-transporter", (req, res) => {
-  const transporter = new Transporters(req.body.latitude, req.body.longitude);
-  const locationTrnasporter = transporter.getPlaces();
+  const trip = new TripInfo(req.body.latitude, req.body.longitude);
+  const locationTrnasporter = trip.getPlaces();
 
-  res.json(locationTrnasporter);
+  res.send(`Returned transporters: ${locationTrnasporter}`);
+});
+
+// api that gives the computed value for distance between in meters
+router.get("/trip-distance", (req, res) => {
+  const trip = new TripMetrix({
+    origin: { lat: req.body.latitude, lng: req.body.longitude },
+    destination: { lat: req.body.latitude, lng: req.body.longitude }
+  });
+  const tripdistance = trip.getTripDistance();
+
+  res.json(tripdistance);
 });
 
 module.exports = router;
