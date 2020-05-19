4a68a34b4353331d77c95639ceeba49e5186f36d
shammah nei <shammahk1@gmail.com>
2020-05-19 22:14:18 +0100

    created reports pages, waiting for api

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Head from "next/head";
 import Homepage from "../components/Homepage/Homepage";
 
 const Home = (props) => {
   return (
     <div>
-      <head>
+      <Head>
         <title>Fasta</title>
-        <link rel="icon" href="/favicon.ico" />
-      </head>
-      <div className="h-screen flex" style={{ backgroundColor: "#F7F5F5" }}>
+        <link rel="icon" href="/images/Logo.png" />
+      </Head>
+      <div className="" >
         <Homepage />
       </div>
     </div>
