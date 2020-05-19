7a19ff663ed30287c1b0debc963ef94927e9e034
kareem fawas <phawazzzy@gmail.com>
2020-05-19 22:51:13 +0100

    created created report route and restricts to only authorized users

--- fasta-backend/app.js
+++ fasta-backend/app.js
@@ -8,6 +8,8 @@ const userRouter = require("./routes/users");
 const indexRouter = require("./routes/index");
 const sendRouter = require("./routes/sendmail");
 const getTransporter = require("./routes/location-transporter");
+const reportRouter = require("./routes/report");
+
 
 require("./db/index");
 // const userRouter = require("./routes/index");
@@ -30,5 +32,6 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/", indexRouter);
 app.use("/api/v1/", sendRouter);
 app.use("/api/v1/", getTransporter);
+app.use("/api/v1", reportRouter);
 
 module.exports = app;
