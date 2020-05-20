60a9b2c23278bf871795e942a554a58d9b8d270d
dhatguy <odunsiolakunbi@gmail.com>
2020-05-20 20:21:29 +0100

    added validations, refactored components

--- components/Account/account.js
+++ components/Account/account.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Router from "next/router";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import BottomNav from "../Homepage/BottomNav";
+import Input from "../Input";
+import { H3 } from "../Text/Headings";
+import { SubmitButton } from "../Buttons";
 
 const NavBar = styled.nav`
   height: 54px;
@@ -21,30 +25,8 @@ const DetailsBody = styled.div`
   margin-bottom: 15px;
 `;
 
-const Button = styled.button`
-  height: 48px;
-  background-color: #43a047;
-  border-radius: 4px;
-  color: #fff;
-  font-size: 12px;
-  font-weight: bold;
-`;
-
-const Input = styled.input`
-  /* width: 60%; */
-  height: 48px;
-  background: #fff;
-  border: 1px solid #afdeb1;
-  padding: 5px;
-  margin-right: 15px;
-  ::placeholder {
-    color: #afdeb1;
-  }
-`;
-
-const Text = styled.p`
-  font-size: 20px;
-  color: #232323;
+const Text = styled(H3)`
+  font-weight: normal;
 `;
 
 export const Nav = (props) => {
@@ -53,55 +35,103 @@ export const Nav = (props) => {
       <div onClick={() => Router.back()}>
         <img src="/images/account/Back-Arrow.svg" alt="back" />
       </div>
-      <Text className="my-0 mx-auto">{props.title}</Text>
+      <Text color="#232323" className="my-0 font-normal mx-auto">{props.title}</Text>
     </NavBar>
   );
 };
 
-const Details = (props) => {
+const Details = ({ name, email }) => {
   return (
     <DetailsBody className="flex items-center">
       <img src="/images/account/profile.svg" alt="profile" />
       <div className="ml-4">
-        <Text>{props.name}</Text>
-        <span style={{ color: "#7f7f7f" }}>{props.email}</span>
+        <Text>{name}</Text>
+        <span style={{ color: "#7f7f7f" }}>{email}</span>
       </div>
     </DetailsBody>
   );
 };
 
-const Number = (props) => {
+const Number = ({number}) => {
+  const [submit, setSubmitted] = useState(false);
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmitForm = (FormData) => setSubmitted(true);
   return (
     <div className="bg-white p-4 mb-4 rounded-lg">
       <div className="flex items-center mb-3">
         <img src="/images/account/phone.svg" alt="phone" />
         <div className="ml-4">
           <Text>Registered Number</Text>
-          <span style={{ color: "#7f7f7f" }}>{props.number}</span>
+          <span style={{ color: "#7f7f7f" }}>{number}</span>
         </div>
       </div>
-      <form>
-        <Input className="w-3/5" type="text" placeholder="New Phone Number" />
-        <Button className="w-2/6" type="submit">
+      <form className="flex justify-around" onSubmit={handleSubmit(onSubmitForm)}>
+        <Input
+          className="w-7/12"
+          type="text"
+          name="phoneNumber"
+          placeholder="New Phone Number"
+          ref={register({
+            required: "Phone number cannot be empty",
+            pattern: {
+              value: /\d{11,14}/,
+              message: "Phone number not valid"
+            }
+          })}
+        />
+        <SubmitButton className="w-2/6" type="submit">
           UPDATE
-        </Button>
+        </SubmitButton>
       </form>
+      {errors.phoneNumber && <p className="text-xs text-red-500 my-2 ml-2">{errors.phoneNumber.message}</p>}
     </div>
   );
 };
 
 const ChangePassword = (props) => {
+  const [submitted, setSubmitted] = useState(false);
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmitForm = (FormData) => {
+    setSubmitted(true);
+  };
   return (
-    <form className="bg-white p-4 rounded-lg mb-4">
+    <form onSubmit={handleSubmit(onSubmitForm)} className="bg-white p-4 rounded-lg mb-4">
       <div className="flex items-start">
         <img src="/images/account/lock.svg" alt="password" />
         <div className="ml-4 flex flex-col justify-around w-full">
           <Text className="mb-4">Change Password</Text>
-          <Input className="w-full mb-3" placeholder="Present Password" />
-          <Input className="w-full mb-3" placeholder="New Password" />
-          <Button className="w-full" type="submit">
+          <Input
+            className="w-full mb-3"
+            placeholder="Present Password"
+            type="password"
+            name="presentPassword"
+            ref={register({
+              required: "Present password required",
+              minLength: {
+                value: 8,
+                message: "Password should be at least 8 characters"
+              }
+            })}
+          />
+          {errors.presentPassword && <p className="text-xs text-red-500 my-2">{errors.presentPassword.message}</p>}
+
+          <Input
+            className="w-full mb-3"
+            placeholder="New Password"
+            type="password"
+            name="newPassword"
+            ref={register({
+              required: "New password required",
+              minLength: {
+                value: 8,
+                message: "New password should be at least 8 characters"
+              }
+            })}
+          />
+          {errors.newPassword && <p className="text-xs text-red-500 my-2">{errors.newPassword.message}</p>}
+          <SubmitButton className="w-full" type="submit">
             CHANGE PASSWORD
-          </Button>
+          </SubmitButton>
         </div>
       </div>
     </form>
@@ -109,18 +139,47 @@ const ChangePassword = (props) => {
 };
 
 const Register = (props) => {
+  const { register, handleSubmit, errors } = useForm();
+  const [submitted, setSubmitted] = useState(false);
+  const onSubmitForm = (FormData) => {
+    setSubmitted(true);
+  };
   return (
-    <form className="bg-white p-4 rounded-lg mb-16">
+    <form onSubmit={handleSubmit(onSubmitForm)} className="bg-white p-4 rounded-lg mb-16">
       <div className="flex items-start">
-        <img src="/images/account/lock.svg" alt="password" />
+        <img src="/images/account/car.png" alt="password" />
         <div className="ml-4 flex flex-col justify-around w-full">
           <Text className="mb-4">Register as a Transporter</Text>
-          <Input className="w-full mb-3" placeholder="Vehicle Type" />
-          <Input className="w-full mb-3" placeholder="Licence Number" />
-          <Input className="w-full mb-3" placeholder="Address" />
-          <Button className="w-full" type="submit">
+          <Input
+            className="w-full mb-3"
+            placeholder="Vehicle Type"
+            name="vehicleType"
+            ref={register({
+              required: "Vehicle type required"
+            })}
+          />
+          {errors.vehicleType && <p className="text-xs text-red-500 my-2">{errors.vehicleType.message}</p>}
+          <Input
+            className="w-full mb-3"
+            placeholder="Licence Number"
+            name="licenceNumber"
+            ref={register({
+              required: "Licence number required"
+            })}
+          />
+          {errors.licenceNumber && <p className="text-xs text-red-500 my-2">{errors.licenceNumber.message}</p>}
+          <Input
+            className="w-full mb-3"
+            placeholder="Address"
+            name="address"
+            ref={register({
+              required: "Address required"
+            })}
+          />
+          {errors.address && <p className="text-xs text-red-500 my-2">{errors.address.message}</p>}
+          <SubmitButton className="w-full" type="submit">
             Register
-          </Button>
+          </SubmitButton>
         </div>
       </div>
     </form>
