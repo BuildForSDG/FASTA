e7b5b8d18e45f55fa7aa2eb83b5818cf2bc75774
myquery <stanarua@aol.com>
2020-06-02 09:23:08 +0100

    rebase repo

---
+++ fasta-backend/api/schedule-api.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+const { Client, Status, latLngToString } = require("@googlemaps/google-maps-services-js");
+const axios = require("axios");
+
+const defaultConfig = {
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+
+// It may be necessary to use a custom axios instance to bypass this for the time being.
+const axiosInstance = axios.create(defaultConfig);
+
+// instantiate the google http client
+const client = new Client({ axiosInstance });
+
+// distance and directions metric
+function TripMetrix(origin, destination) {
+  this.origin = origin;
+  this.destination = destination;
+  console.log(origin.lat);
+}
+
+
+// api to get the distance of the trip from the origin to the destination
+TripMetrix.prototype.getTripDistance = async () => {
+//   const latLngOrigin = latLngToString(this.origin);
+//   const latLngDestination = latLngToString(this.destination);
+  console.log(this.origin);
+  client.directions({
+
+    params: {
+      origin: [{ lat: this.origin.lat, lng: this.origin.lng }],
+      destination: [{ lat: this.destination.lat, lng: this.destination.lng }],
+      key: process.env.TEST_KEY,
+      mode: "driving",
+      traffic_model: "best_guess",
+      departure_time: 1589673600
+    }
+  })
+    .then((r) => {
+      if (r.data.status === Status.OK) {
+        console.log(r.data);
+      } else {
+        console.log(r.data.error_message);
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+
+module.exports = TripMetrix;
--- fasta-backend/routes/report.js
+++ fasta-backend/routes/report.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 const express = require("express");
 
 const router = express.Router();
