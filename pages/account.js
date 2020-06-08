ca52144a323b83572c9d7f03b1275cfebc1ca631
CM <52335952+danieletchie@users.noreply.github.com>
2020-06-08 22:06:22 +0100

    More codacy errors

--- pages/account.js
+++ pages/account.js
@@ -3,7 +3,7 @@ import React from "react";
 import Head from "next/head";
 import Account from "../components/Account/account";
 
-const Profile = props => {
+const Profile = (props) => {
   return (
     <div>
       <Head>
