a13d9b97400d3d40eef7945b3f25ad58ccda7b7e
obafemi oderanti <thevetdoctor.gmail.com>
2020-06-09 10:50:01 +0100

    update trip schema

--- fasta-backend/routes/location-transporter.js
+++ fasta-backend/routes/location-transporter.js
@@ -90,7 +90,7 @@ router.post("/trip-direction-info", async (req, res) => {
 
 router.post("/schedule-a-trip", authChecker, async (req, res) => {
   const {
-    mode, origin, destination, isVulnerable, tripDistance, tripTime
+    mode, origin, originLatLng, originLocation, destination, destinationLatLng, destinationLocation, isVulnerable, tripDistance, tripDuration, tripTime
   } = req.body;
 
   if (!mode || !origin || !destination || !isVulnerable || !tripDistance || !tripTime) {
@@ -101,9 +101,14 @@ router.post("/schedule-a-trip", authChecker, async (req, res) => {
     const tripDetails = {
       mode,
       origin,
+      originLatLng,
+      originLocation,
       destination,
+      destinationLatLng,
+      destinationLocation,
       isVulnerable,
       tripDistance,
+      tripDuration,
       tripTime,
       userId: req.user._id
     };
@@ -119,12 +124,12 @@ router.post("/schedule-a-trip", authChecker, async (req, res) => {
 // add the authChecker for authentication before, endpoint will list all the schecduled trip
 router.get("/trips", authChecker, async (req, res) => {
   await ScheduleTrip.find({ userId: req.user._id})
-    .select("_id mode origin destination isVulnerable tripDistance tripTime date")
+    .select()
     .exec()
     .then((allTrips) => {
       if (!allTrips || allTrips < 1) {
         console.log(allTrips);
-        return res.status(404).json({ response: "unfortunetly, we dont have any trips schedule for you, check back" });
+        return res.status(404).json({ response: "Unfortunately, we dont have any trips scheduled for you, please check back" });
       }
       res.status(200).json({ response: allTrips.reverse() });
     })
@@ -161,15 +166,7 @@ router.get("/trips/:id", authChecker, async (req, res) => {
         return res.status(404).json({ response: "This report doesn't exist anymore" });
       }
       return res.status(200).json({
-        response: {
-          mode: trip.mode,
-          origin: trip.origin,
-          destination: trip.destination,
-          isVulnerable: trip.isVulnerable,
-          tripDistance: trip.tripDistance,
-          tripTime: trip.tripTime,
-          date: trip.date
-        }
+        response: trip
       });
     }).catch((e) => {
       res.status(500).json({ e: e.message });
