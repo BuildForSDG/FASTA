2a6b1cbcb31fdb1949f3db537a84c1ce29817917
obafemi <thevetdoctor@gmail.com>
2020-07-09 06:32:37 +0100

    refactor and fix trip info route

--- fasta-backend/routes/tripInfo.js
+++ fasta-backend/routes/tripInfo.js
@@ -1,69 +1,110 @@
+/* eslint-disable no-useless-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-plusplus */
+/* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 /* eslint-disable consistent-return */
 const express = require("express");
-// const { PolyUtil } = require("node-geometry-library");
+const axios = require("axios");
+const router = express.Router();
+const { PolyUtil } = require("node-geometry-library");
 const ScheduleTrip = require("../models/trip");
 const Reports = require("../models/report");
 
-const router = express.Router();
 
-router.get("/trip-info/:tripId", async (req, res) => {
-// get allthe report locations
-  const reports = [];
-  let trips;
-  await Reports.find()
-    .select("-_id location")
-    .exec()
-    .then((allReports) => {
-      if (!allReports || allReports < 1) {
-        return res.status(404).json({ response: "unfortunetly, we dont have any report location schedule for you, check back" });
-      }
+const directionUrl = "https://maps.googleapis.com/maps/api/directions/json?";
 
-      const valuesArray = Object.keys(allReports);
-      valuesArray.forEach((key) => {
-        const value = allReports[parseInt(key)];
-        reports.push(value.location);
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
 
-  // get schedule trip Id
-  await ScheduleTrip.findById({ _id: req.params.tripId })
-    .select("-_id origin destination")
-    .exec()
-    .then((trip) => {
-      if (!trip || trip < 1) {
-        return res.status(404).json({ response: "unfortunetly, we dont have any report location schedule for you, check back" });
-      }
-      trips = trip;
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+router.get("/trip-info/:tripId?", async (req, res) => {
 
-  try {
-    const response = await PolyUtil.containsLocation(trips, reports);
-    if (response) {
-      await Reports.find()
-        .select()
-        .exec()
-        .then((allReports) => {
-          if (!allReports || allReports < 1) {
-            return res.status(404).json({ response: "unfortunetly, we dont have any report location schedule for you, check back" });
+  console.log("params: ", req.params);
+  console.log("query: ", req.query);
+  let { t } = req.query;
+  console.log("t: ", Number(t));
+  const tolerance = Number(t) || PolyUtil.DEFAULT_TOLERANCE;
+  let reportsOnRoute;
+
+    // identify trip by ID from database
+  const tripz = await ScheduleTrip.findById({ _id: req.params.tripId })
+                                  .select("-_id mode originLatLng destinationLatLng")
+                                  .exec()
+                                  .then((trip) => {
+                                    if (!trip || trip < 1) {
+                                      return res.json({response: "Trip not found!"});
+                                    }
+                                    console.log("trip: ", trip);
+                                    return trip; 
+                                  })
+                                  .catch((error) => {
+                                    return res.status(500).json({ error });
+                                  });
+  
+    // pull reports from database
+  const reportz = await Reports.find()
+                                .select("-_id")
+                                .exec()
+                                .then((allReports) => {
+                                  if (!allReports || allReports < 1) {
+                                    return res.status(404).json({ response: "Reports not available at the moment, we're still checking" });
+                                  }
+                                  // console.log("allReports: ", allReports);
+                                  return allReports;
+                                })
+                                .catch((error) => {
+                                  return res.status(500).json({ error });
+                                });
+                            
+  // get list of points between from origin to destination
+  const getDirection = async (request) => {
+                        const origin = `${request.originLatLng.lat},${request.originLatLng.lng}`;
+                        const destination = `${request.destinationLatLng.lat},${request.destinationLatLng.lng}`;
+                        // console.log(origin, destination);
+                        return await axios
+                          .get(
+                            `${directionUrl}origin=${origin}&destination=${destination}&key=${process.env.TEST_KEY}`
+                          )
+                          .then((response) => {
+                            try {
+                              if (response.data.length <= 0) {
+                                return res.json({ error: response.data.error_message });
+                              }
+                              // console.log("steps: ", response.data.routes[0].legs[0].steps);
+                              // console.log("points: ", response.data.routes[0].overview_polyline.points);
+                              return response.data.routes[0].overview_polyline.points;
+                            } catch (err) {
+                              console.error(err);
+                            } 
+                          })
+                          .catch((error) => {
+                            // throw new Error("Error fetching data");
+                            return res.json({response: "Error fetching data"});
+                          });
+                        };
+
+      try {
+        const triped = JSON.parse(JSON.stringify(tripz));
+        const response = await getDirection(triped);
+        const path = PolyUtil.decode(response);
+        console.log("PolyUtil decoded points: ", path.length);
+           
+        const reportsOnPath = reportz.filter((report) => {
+          if (report.location !== undefined && report.location !== "unknown location") {
+            const reportFound = PolyUtil.isLocationOnEdge(report.location, path, tolerance);
+            if (reportFound) {
+              // console.log("PolyUtil tolerance", tolerance);
+              console.log(report.location,"is on path");
+              return report;
+            }
           }
-          res.status(200).json({ response: allReports });
-        })
-        .catch((error) => {
-          res.status(500).json({ error });
-        });
-    } else {
-      res.send("Trip does match any incidence");
-    }
-  } catch (err) {
-    // empty
-  }
+        }); 
+        reportsOnRoute = reportsOnPath; 
+
+      } catch (err) {
+        console.log(err);
+      }
+      // console.log(reportsOnRoute);
+
+    return res.json({response: reportsOnRoute});
 });
 
 module.exports = router;
